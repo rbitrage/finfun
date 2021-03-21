975fdd941f3755d66c3030ce415522f921ce6b87
rbitrage <rbitrage@economix.at>
2021-03-21 15:23:54 +0100

    roxygen Dokumentation

--- R/price.R
+++ R/price.R
@@ -1,3 +1,16 @@
+#' Preis einer Anleihe
+#'
+#' Berechnet den Preis einer Anleihe auf Basis der uebergebenen Cashflows und
+#' Yield-to-Maturity.
+#'
+#' @param cash_flows Vektor der Cashflows
+#' @param ytm Yield-to-Maturity in Dezimalform
+#'
+#' @return Preis der Anleihe
+#' @export
+#'
+#' @examples
+#' price(c(-95, 2, 2, 2, 2, 102), 0.01)
 price <- function(cash_flows, ytm) {
   t <- base::seq(from = 0, along.with = cash_flows)
   sum(cash_flows / (1 + ytm)^t)
--- R/ytm.R
+++ R/ytm.R
@@ -1,3 +1,17 @@
+#' Yield-to-Maturity
+#'
+#' Berechnet die Yield-to-Maturity (Endfaelligkeitsrendite) einer Anleihe auf
+#' Basis der uebergebenen Cashflows. Die Approximation erfolgt mittels
+#' stats::uniroot() im Intervall -1:1, wodurch auch negative Renditen zulaessig
+#' sind.
+#'
+#' @param cash_flows Vektor der Cashflows
+#'
+#' @return Yield-to-Maturity in Dezimalform
+#' @export
+#'
+#' @examples
+#' ytm(c(-95, 2, 2, 2, 2, 102))
 ytm <- function(cash_flows) {
   stats::uniroot(f = price,
                  interval = c(-1, 1),
